11fb8fee5fe383cd95161aea30aba34e63a082d9
Carl John Cruz <jy081320@gmail.com>
2023-01-08 12:00:32 +0800

    WebSystem  Lab A Done

---
+++ Employee.js
@@ -0,0 +1,8 @@
+const findEmp = (data) => {
+    const emps = JSON.parse(data);
+    const names = emps.map((emp) => emp.employee);
+    console.log(`Employee: ${names.join(", ")}`);
+  };
+  
+  module.exports = findEmp;
+  
